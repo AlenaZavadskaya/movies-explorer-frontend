4405405084a907992c54d1a83ebb17512a7f2ae4
Alena Zavadskaya <alena1yakusheva@gmail.com>
2021-03-15 20:28:11 +0300

    filter checkbox

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./FilterCheckbox.css";
 
-function FilterCheckbox() {
+function FilterCheckbox(props) {
   return (
     <section className="filter-checkbox">
-      <button type="button" className="filter-checkbox__handler" />
-      <p className="filter-checkbox__option">Короткометражки</p>
+      <label className="search-form__filter-container">
+        Короткометражки
+        <input
+          type="checkbox"
+          name="short-movies"
+          id="short-movies"
+          className="search-form__filter-checkbox"
+          onChange={props.onFilter}
+          checked={props.isShortMovie}
+        />
+        <span className="search-form__filter-checkmark"></span>
+      </label>
     </section>
   );
 }
