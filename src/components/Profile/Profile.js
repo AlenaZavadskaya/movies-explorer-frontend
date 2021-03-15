d52622b72cb5419065e6e684b7698849730480f7
Alena Zavadskaya <alena1yakusheva@gmail.com>
2021-03-15 09:52:18 +0300

    validate form

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import "../Profile/Profile.css";
 import Header from "../Header/Header";
@@ -6,20 +6,93 @@ import Navigation from "../Navigation/Navigation";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 function Profile(props) {
-  const currentUser = React.useContext(CurrentUserContext);
+  const currentUser = useContext(CurrentUserContext);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [changedName, setChangedName] = useState(false);
+  const [changedEmail, setChangedEmail] = useState(false);
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [isInputDisabled, setIsInputDisabled] = useState(true);
+  const [formValid, setFormValid] = useState(false);
 
+  useEffect(() => {
+    if (currentUser.name !== undefined) {
+      setName(currentUser.name);
+      setEmail(currentUser.email);
+    }
+  }, [currentUser]);
+
+  function handleNameChange(e) {
+    setChangedName(true);
+    const validName = /^[a-zA-Z- ]+$/.test(e.target.value);
+
+    if (e.target.value.length < 2) {
+      setNameError("Длина имени должна быть не менее 2 символов");
+    } else if (e.target.value.length > 30) {
+      setNameError("Длина имени должна должна быть не более 30 символов");
+    } else if (!validName) {
+      setNameError("Имя должно быть указано латиницей");
+    } else {
+      setNameError("");
+    }
+    setName(e.target.value);
+  }
+
+  function handleEmailChange(e) {
+    setChangedEmail(true);
+    const validEmail = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i.test(
+      e.target.value
+    );
+
+    if (!validEmail) {
+      setEmailError("Неверный формат почты");
+    } else {
+      setEmailError("");
+    }
+    setEmail(e.target.value);
+  }
+
+  function changeInputDisabled() {
+    setIsInputDisabled(!isInputDisabled);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.onEditUser({
+      name,
+      email,
+    });
+    changeInputDisabled();
+  }
+
+  useEffect(() => {
+    setName(currentUser.name);
+    setEmail(currentUser.email);
+  }, [currentUser]);
+
+  useEffect(() => {
+    if (nameError || emailError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [nameError, emailError]);
+
+  useEffect(() => {
+    if (currentUser.name === name && currentUser.email === email) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [name, email, currentUser.name, currentUser.email]);
   return (
     <>
       <Header className="header header__white">
         <Navigation onClick={props.onMenu} />
       </Header>
       <section className="profile">
-        <form
-          className="form__profile"
-          id="profile"
-          onSubmit={props.onSubmit}
-          noValidate
-        >
+        <form className="form__profile" id="profile" onSubmit={handleSubmit}>
           <div className="form__container_auth">
             <h2 className="form__heading-profile">{`Привет, ${currentUser.name}!`}</h2>
             <fieldset className="form__inputs">
@@ -28,37 +101,47 @@ function Profile(props) {
                   Имя
                   <input
                     id="profile-name"
-                    className="form__item form__item-profile"
+                    className={`form__item form__item-profile ${
+                      changedName && nameError ? "form__item-profile_error" : ""
+                    }`}
                     type="text"
-                    placeholder="Имя"
-                    minLength="2"
-                    maxLength="40"
-                    defaultValue={currentUser.name}
+                    value={name}
+                    onChange={handleNameChange}
+                    disabled={!isInputDisabled}
                   />
-                  <span id="name-input-error" className="form__item-error">
-                    {props.message}
-                  </span>
                 </label>
               </div>
-              <div className="form__input-container-profile">
+              <span className="form__item-profile_error form__profile_span">
+                {nameError}
+              </span>
+              <div className="form__input-container-profile form__input-container_border">
                 <label className="form__field-profile">
                   Почта
                   <input
                     id="profile-email"
-                    className="form__item form__item-profile"
+                    className={`form__item form__item-profile ${
+                      changedEmail && emailError
+                        ? "form__item-profile_error"
+                        : ""
+                    }`}
                     type="text"
-                    placeholder="Почта"
-                    minLength="2"
-                    maxLength="40"
-                    defaultValue={currentUser.email}
+                    value={email}
+                    onChange={handleEmailChange}
+                    disabled={!isInputDisabled}
                   />
-                  <span id="name-input-error" className="form__item-error">
-                    {props.message}
-                  </span>
                 </label>
               </div>
+              <span className="form__item-profile_error">{emailError}</span>
               <div className="form__handlers">
-                <button className="submit__button-profile" type="submit">
+                <div className="form__item-message">{props.message}</div>
+                <button
+                  className={`submit__button-profile ${
+                    !formValid ? "submit__button-profile_disabled" : ""
+                  }`}
+                  type="submit"
+                  disabled={!formValid}
+                  onClick={changeInputDisabled}
+                >
                   Редактировать
                 </button>
               </div>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,38 +3,68 @@ import "./SearchForm.css";
 
 function SearchForm(props) {
   const [findedMovie, setFindedMovie] = useState("");
+  const [error, setError] = React.useState("");
+  const [formValid, setFormValid] = React.useState(false);
+
   function handleSearchMovie(e) {
     setFindedMovie(e.target.value);
+    if (e.target.value.length === 0) {
+      setError("Нужно ввести ключевое слово");
+    } else {
+      setError("");
+    }
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.onGetMovies();
+    setError("");
+    props.onGetMovies(findedMovie);
     setFindedMovie("");
   }
 
+  React.useEffect(() => {
+    if (findedMovie && !error) {
+      setFormValid(true);
+    } else {
+      setFormValid(false);
+    }
+  }, [findedMovie, error]);
+
   return (
-    <section className="search-form" onSubmit={handleSubmit}>
-      <div className="search-form__container">
-        <div className="search-form__search-icon" />
-        <form className="search-form__form" noValidate>
-          <label className="search-form__field">
-            <input
-              className="search-form__input"
-              type="text"
-              name="search"
-              placeholder="Фильм"
-              minLength="2"
-              maxLength="40"
-              value={findedMovie}
-              onChange={handleSearchMovie}
-              required
-            />
-          </label>
-        </form>
-        <button className="submit__button" type="submit"></button>
-      </div>
-    </section>
+    <>
+      <section className="search-form">
+        <div className="search-form__container">
+          <button
+            type="submit"
+            className="search-form__search-icon"
+            onClick={handleSubmit}
+            disabled={!formValid}
+          />
+          <form className="search-form__form" onSubmit={handleSubmit}>
+            <label className="search-form__field">
+              <input
+                className="search-form__input"
+                type="text"
+                name="search"
+                placeholder="Фильм"
+                minLength="2"
+                maxLength="40"
+                value={findedMovie}
+                onChange={handleSearchMovie}
+                required
+              />
+            </label>
+          </form>
+          <button
+            className="submit__button"
+            type="submit"
+            onClick={handleSubmit}
+            disabled={!formValid}
+          ></button>
+        </div>
+        <div className="form__item-error">{error}</div>
+      </section>
+    </>
   );
 }
 
